8677ec2bd8d1adf9c2b836fa3132e208d6f4f7bb
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-05 22:36:09 +0700

    Switch Statement

---
+++ SwitchStatement.php
@@ -0,0 +1,15 @@
+<?php
+
+$nilai = "C";
+
+switch ($nilai) {
+    case "A":
+        echo "Anda Lulus" . PHP_EOL;
+        break;
+    case "B":
+        echo "NILAI LU B ";
+        break;
+    case "C":
+        echo "Niali lu C ";
+        break;
+}
