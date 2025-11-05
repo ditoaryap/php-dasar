5352158668ee836b7f66545dc1d3e7cc1d43ff5b
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-05 22:26:37 +0700

    manipulation string int float convert

--- ManipulasiString.php
+++ ManipulasiString.php
@@ -3,3 +3,12 @@
 $name = "Dito Aryaputra Ramadhani";
 
 echo "Name = " . $name . PHP_EOL;
+
+$valueString = (string) 100;
+var_dump($valueString);
+
+$valueInt = (int) "100";
+var_dump($valueInt);
+
+$valueFloat = (float) "1.01";
+var_dump($valueFloat);
