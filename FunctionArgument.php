81ebc6c538f15f8ff37528b31358956ace1a5984
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-06 04:02:23 +0700

    function argument 3

--- FunctionArgument.php
+++ FunctionArgument.php
@@ -17,4 +17,18 @@ function sum(int $first, int $last)
 sum(100, 100);
 sum("100", "100");
 sum(true, false);
-sum([], []);
+// sum([], []);
+
+function sumAll(...$values)
+{
+    $total = 0;
+    foreach ($values as $value) {
+        $total += $value;
+    }
+    echo "Total " . implode(",", $values) . " = $total" . PHP_EOL;
+}
+
+$values = [1, 2, 3, 4, 5];
+
+sumAll(1, 2, 3, 4, 5);
+sumAll(...$values);
