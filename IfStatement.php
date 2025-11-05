fd2c2e0d37c85462209d47ad1eef8daa4c1da02e
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-05 22:33:45 +0700

    if statement part 1

---
+++ IfStatement.php
@@ -0,0 +1,10 @@
+<?php
+
+$nilai = 72;
+$absen = 90;
+
+if ($nilai >= 75 && $absen >= 75) {
+    echo "Selamat Anda Lulus" . PHP_EOL;
+} else {
+    echo "anjay" . PHP_EOL;
+}
--- ManipulasiString.php
+++ ManipulasiString.php
@@ -12,3 +12,14 @@ var_dump($valueInt);
 
 $valueFloat = (float) "1.01";
 var_dump($valueFloat);
+
+$name = "Dito";
+echo $name[0] . PHP_EOL;
+echo $name[1] . PHP_EOL;
+echo $name[2] . PHP_EOL;
+echo $name[3] . PHP_EOL;
+
+echo "Hello $name" . PHP_EOL;
+
+$var = "var";
+echo "This is {$var}s" . PHP_EOL;
