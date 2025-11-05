b3520d96c9d091ee706b10a58c7acab20d7a5577
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-06 04:14:05 +0700

    Functon Return Value

---
+++ FunctionReturnValue.php
@@ -0,0 +1,32 @@
+<?php
+
+function sum(int $first, int $second)
+{
+    $total = $first + $second;
+    return $total;
+}
+
+$result = sum(10, 10);
+var_dump($result);
+
+$result = sum(100, 100);
+var_dump($result);
+
+function getFinalValue(int $value)
+{
+    if ($value >= 80) {
+        return "A";
+    } elseif ($value >= 70) {
+        return "B";
+    } elseif ($value >= 60) {
+        return "C";
+    } elseif ($value >= 50) {
+        return "D";
+    } else {
+        return "E";
+    }
+    // echo "Ups" . PHP_EOL;
+}
+
+$score = getFinalValue(30);
+var_dump($score);
