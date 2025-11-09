fb22ec0f1f7592c45d11aee5258da91e4cfcfdf1
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-10 00:27:09 +0700

    string function

---
+++ StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [10, 11, 12, 13, 14, 15]));
+var_dump(explode(" ", "Dito Aryaputra Ramadhani"));
+var_dump(strtoupper("dito aryaputra ramadhani"));
+var_dump(strtolower("DITO ARYAPUTRA RAMADHANI"));
+var_dump(trim("      dito     "));
+var_dump(substr("Dito Aryaputra Ramadhani", 0, 4));
