516ecbb8e9aa1880ef1414d9b75472b92f87794e
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-12 00:09:11 +0700

    Properties php

--- php-oop/Properties.php
+++ php-oop/Properties.php
@@ -12,3 +12,10 @@ var_dump($person);
 echo "Name = $person->name" . PHP_EOL;
 echo "Name = $person->address" . PHP_EOL;
 echo "Name = $person->country" . PHP_EOL;
+
+$person2 = new Person();
+$person2->name = "Aji";
+$person2->address = "asdasd";
+$person2->country = "zxccbxvvcb";
+
+var_dump($person2);
