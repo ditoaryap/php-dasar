9a705a673dbc4704f996630264d1bb48052826aa
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-12 11:29:07 +0700

    function person

---
+++ php-oop/Function.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "data/Person.php";
+
+$dito = new Person();
+$dito->name = "JOJO";
+$dito->sayHello("dito");
+
+$joko = new Person();
+$joko->name = "JOJO";
+$joko->sayHello(null);
--- php-oop/data/Person.php
+++ php-oop/data/Person.php
@@ -5,4 +5,13 @@ class Person
     var string $name;
     var ?string $address = null;
     var string $country = "Indonesia";
+
+    function sayHello(?string $name)
+    {
+        if(is_null($name)){
+            echo "Hi, my name is $this->name" . PHP_EOL;
+        } else {
+            echo "Hi $name my name is $this->name" . PHP_EOL;
+        }
+    }
 }
