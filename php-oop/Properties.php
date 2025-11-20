4c6aeb513fc6f8fa35c2bcfc8366eacf7ee17da7
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-20 21:58:15 +0800

    php-oop/Properties.php

--- php-oop/Properties.php
+++ php-oop/Properties.php
@@ -4,21 +4,16 @@ require_once "data/Person.php";
 
 $person = new Person();
 $person->name = "Dito";
-$person->address = "Depok";
-// $person->country = "Indonesia";
+$person->address = "Citayem";
+$person->country = "Indonesia";
 
 var_dump($person);
 
-echo "Name = $person->name" . PHP_EOL;
-echo "Name = $person->address" . PHP_EOL;
-echo "Name = $person->country" . PHP_EOL;
+echo "Name : $person->name" . PHP_EOL;
+echo "Address : $person->address" . PHP_EOL;
+echo "Country : $person->country" . PHP_EOL;
 
 $person2 = new Person();
-$person2->name = "Aji";
-$person2->address = "asdasd";
-// $person2->country = "zxccbxvvcb";
-
-var_dump($person2);
-
-// error
-// $person2->name = [];
+$person2->name = "Budi";
+$person2->address = "Jakarta";
+$person2->country = "Indonesia";
