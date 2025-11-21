51e14bcdac3a43e0bc02dfb868c7b3984523d269
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-21 21:54:33 +0800

    constructor overiding

--- php-oop/data/Manager.php
+++ php-oop/data/Manager.php
@@ -4,6 +4,13 @@ class Manager
 {
     var string $name;
 
+    var string $title;
+
+    public function __construct(string $name = "", string $title = "Manager"){
+        $this->name = $name;
+        $this->title = $title;
+    }
+
     function sayHello(string $name): void
     {
         echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
@@ -11,6 +18,13 @@ class Manager
 }
 
 class VicePresident extends Manager {
+
+    public function __construct(string $name = "")
+    {
+        // tidak wajib, tapi direkomendasikan
+        parent::__construct($name, "VP");
+    }
+
     function sayHello(string $name): void
     {
         echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
